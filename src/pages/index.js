eed3ba8af12ca732f503bc64a8700b931dbba082
ClementVion <clementvion1@gmail.com>
2022-09-08 12:09:22 +0200

    Change title

--- src/pages/index.js
+++ src/pages/index.js
@@ -74,4 +74,4 @@ const IndexPage = () => {
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Clément Vion – Freelance Creative Developer</title>
